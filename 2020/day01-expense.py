c52fe0aa76f777d1957f911d04533c0ca1fbf8ab
Gabriel Augendre <gabriel@augendre.info>
2020-12-01 09:14:04 +0100

    Complete 2020 day 1 part 2

--- 2020/day01-expense.py
+++ 2020/day01-expense.py
@@ -8,9 +8,11 @@ def main():
     for i, value in enumerate(lines):
         for j in range(i+1, stop):
             other = lines[j]
-            addition = other + value
-            if addition == 2020:
-                print("result is", other * value)
+            for k in range(j+1, stop):
+                third = lines[k]
+                addition = other + value + third
+                if addition == 2020:
+                    print("result is", other * value * third)
 
 
 if __name__ == '__main__':
