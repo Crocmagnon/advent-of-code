21272f7281b2111a1817b96d565f8a865a73a427
Gabriel Augendre <gabriel@augendre.info>
2021-12-02 20:03:47 +0100

    Solve day 1 part 2

--- 2021/day01.js
+++ 2021/day01.js
@@ -1,4 +1,4 @@
 let total = 0;
-$("pre").innerText.trim().split("\n").map(el => Number(el)).reduce((prev, current) => {if (prev < current) {total += 1} return current});
+$("pre").innerText.trim().split("\n").map(el => Number(el)).map((el, index, array) => el + array[index+1] + array[index+2]).reduce((prev, current) => {if (prev < current) {total += 1} return current});
 console.log(total);
 
