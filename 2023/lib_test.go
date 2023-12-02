c554a5e463db3a2f4c4920fe47133df40b3cc1a8
Gabriel Augendre <gabriel@augendre.info>
2023-12-02 13:57:21 +0100

    fix new day script

---
+++ 2023/lib_test.go
@@ -0,0 +1,18 @@
+package _023
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func check(test testCase, fn solveFunc) func(t *testing.T) {
+	return func(t *testing.T) {
+		file, err := os.Open(test.filename)
+		require.NoError(t, err)
+		got, err := fn(file)
+		require.NoError(t, err)
+		assert.Equal(t, test.want, got)
+	}
+}
--- 2023/template_test.go
+++ 2023/template_test.go
@@ -1,9 +1,6 @@
 package _023
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"os"
 	"testing"
 )
 
@@ -24,15 +21,5 @@ func TestDxxPart2(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.filename, check(test, DxxPart2))
-	}
-}
-
-func check(test testCase, fn solveFunc) func(t *testing.T) {
-	return func(t *testing.T) {
-		file, err := os.Open(test.filename)
-		require.NoError(t, err)
-		got, err := fn(file)
-		require.NoError(t, err)
-		assert.Equal(t, test.want, got)
 	}
 }
